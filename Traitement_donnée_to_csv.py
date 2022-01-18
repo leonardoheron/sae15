c7d80d792118d3d43d12623d63cc5086fd25110d
leonardoheron <96128400+leonardoheron@users.noreply.github.com>
2022-01-18 22:06:55 +0100

    Update Traitement_donnée_to_csv.py

--- Traitement_donnée_to_csv.py
+++ Traitement_donnée_to_csv.py
@@ -1,4 +1,7 @@
 import os
+import markdown
+import matplotlib.pyplot as plt
+
 
 def trad(tab):
     """
@@ -21,6 +24,20 @@ def trad(tab):
         tab[6] = "FIN ACK"
     return tab
 
+def supp_ports(line):
+    if "BP-Linux8" in line[2]:
+        line[2] = "BP-Linux8"
+    if "BP-Linux8" in line[4]:
+        line[4] = "BP-Linux8"
+    if "gba.solunet.com" in line[2]:
+        line[2] = "gba.solunet.com"
+    if "gba.solunet.com" in line[4]:
+        line[4] = "gba.solunet.com"
+    if "1e100.net.https" in line[2]:
+        line[2] = "1e100.net.https"
+    if "1e100.net.https" in line[4]:
+        line[4] = "1e100.net.https"
+    return line
 
 def fnct(chemin: str):
     """
@@ -34,17 +51,15 @@ def fnct(chemin: str):
         print("Le fichier n'existe pas %s", os.path.abspath(chemin))
         
     texte_decoup = files.split('\n')
-        
+    tab_ip = []
+    tab_occu =[] 
     for event in texte_decoup:
         # Initialisation chaine de carcatere
         if event.startswith('11:42'):
             line = event.split()
             trad(line)
+            line = supp_ports(line)
             if line[5] == "Flags":
-                if "BP-Linux8" in line[2]:
-                    line[2] = "BP-Linux8"
-                if "BP-Linux8" in line[4]:
-                    line[4] = "BP-Linux8"
                 evenement='temps : '+line[0]+';'+' Adresse Ip source : '+line[2]+';'+' Adresse IP destinataire : '+line[4]+';'+' flag : '+line[6]+';'
                 if line[6] == "SYN":
                     evenement += 'Numéro de séquence : '+line[8]+';'+' Taille de la fenêtre : '+line[10]+';'+' Longueur du paquet : '+line[len(line)-2]+';'+'Protocole :' +line[len(line)-1]+';'
@@ -66,12 +81,53 @@ def fnct(chemin: str):
             if line[5] == 'ICMP':
                 evenement='temps : '+line[0]+';'+' Adresse Ip source : '+line[2]+';'+' Adresse IP destinataire : '+line[4]+';'+' Protocole : '+line[5]+';'+'Status :'+line[6]+';'+'State :'+line[7]+';'+'Id :'+line[9]+';'
             
+            if line[2] not in tab_ip:
+                tab_ip.append(line[2])
+                tab_occu.append(1)
+            else:
+                for i in range(len(tab_ip)):
+                    if tab_ip[i] == line[2]:
+                        tab_occu[i] += 1
+
+            if line[4] not in tab_ip:
+                tab_ip.append(line[4])
+                tab_occu.append(1)
+            else:
+                for i in range(len(tab_ip)):
+                    if tab_ip[i] == line[4]:
+                        tab_occu[i] += 1
+            tab_ip_final = []
+            tab_occu_final = []
+            for i in range(len(tab_occu)):
+                if tab_occu[i] >=100:
+                    tab_ip_final.append(tab_ip[i])
+                    tab_occu_final.append(tab_occu[i])
+
             with open('fichier.csv','a') as f:
                 f.write(f"{evenement}\n")
+    rep = int(input("Souhaitez-vous un diagramme camambert ? 1 : oui 2 : non"))
+    if rep == 1:
+        camambert(tab_ip_final, tab_occu_final)
+        website()
     f.close()
     fh.close()
     print('Fichier CSV créé avec succès')
     
+def camambert(tab_ip,tab_occu):
+    plt.pie(tab_occu, labels=tab_ip, autopct='%1.1f%%', shadow=True, startangle=90) 
+    plt.axis('equal')
+    plt.savefig('result.png')
+    plt.show()
+
+def website():
+    with open("test.html","w") as f:
+        f.write(markdown.markdown('''#<center>Diagramme d'occurences des IPs</center>
+<center>![diagramme](result.png)</center>
+##<center>Défaillance ICMP</center>
+<center>![icmp](icmp.png)</center>
+'''))
+    f.close()
+
 if __name__=="__main__":
     val = input("Entrer le chemin d'accès de votre fichier que vous souhaitez traiter : ")
     fnct(val)
